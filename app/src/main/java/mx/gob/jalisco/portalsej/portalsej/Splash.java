3fafaca307189eefa8d9e8803e81bffd7063b545
alejandrogar <manyoh.3y@gmail.com>
2016-06-13 17:50:35 -0500

    Se añadió un Splash screen

---
+++ app/src/main/java/mx/gob/jalisco/portalsej/portalsej/Splash.java
@@ -0,0 +1,69 @@
+package mx.gob.jalisco.portalsej.portalsej;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Splash extends Activity {
+
+    private static final long SPLASH_SCREEN_DELAY = 3000;
+
+    Context context;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        context = Splash.this;
+        // Set portrait orientation
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        // Hide title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        setContentView(R.layout.activity_splash);
+
+        View decorView = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+        //Random rand = new Random();
+
+        //int image = rand.nextInt((3) + 1);
+
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+                // Start the next activity
+                Intent mainIntent = new Intent().setClass(
+                        Splash.this, MainActivity.class);
+                startActivity(mainIntent);
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+                finish();
+
+
+                // Close the activity so the user won't able to go back this
+                // activity pressing Back button
+
+            }
+        };
+
+        // Simulate a long loading process on application startup.
+        Timer timer = new Timer();
+        timer.schedule(task, SPLASH_SCREEN_DELAY);
+    }
+}
