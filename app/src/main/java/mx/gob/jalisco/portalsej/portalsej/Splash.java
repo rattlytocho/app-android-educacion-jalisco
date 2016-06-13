3365788b0cd0f081338dd7eaef351f0cdc22e510
alejandrogar <manyoh.3y@gmail.com>
2016-06-13 18:01:38 -0500

    Se añadió un Splash screen

--- app/src/main/java/mx/gob/jalisco/portalsej/portalsej/Splash.java
+++ app/src/main/java/mx/gob/jalisco/portalsej/portalsej/Splash.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
